5e2f8c4db48469cedf2c5db9a5bd66a78cb82850
Garrett Gonzales <gggonzo4@yahoo.com>
2023-12-20 22:41:47 -0800

    fix for heroku

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
 
 app.get('/notes', function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
+  res.sendFile(path.join(__dirname, "./notes.html"));
 });
 
 app.get('/notes', function (req, res) {
